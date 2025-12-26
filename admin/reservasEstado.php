b1a8fa2344db3750c65befd797b4fdfaa15585de
SisteMANZ <admin@sistemanz.com.ar>
2025-12-26 16:57:54 -0300

    fix: voucher solo en reservas pasadas confirmadas, no en pendientes
    - Removido botón de voucher de 'reservas pasadas pendientes'
    - Mantenido botón de voucher en 'reservas confirmadas pasadas'
    - Admin ve todas las reservas (futuras y pasadas) en ambas secciones

--- admin/reservasEstado.php
+++ admin/reservasEstado.php
@@ -250,15 +250,6 @@ $reservas=getReservasConfirmadas($idPrestador, $vistaAdmin);
 
                                     <td><?=$_SESSION["moneda_sel_sym"].$total;?></td>
 
-                                    <td><form method="post" action="voucherPrestador"><button type="submit" class="btn btn-info" name="idReservaHorarios" value="<?=$idReservaHorarios;?>"><?=$lang["voucher_prestador"];?></button></form></td>
-
-                             
-
-
-
-                             </tr>
-
-
 
                              <?php
 
@@ -520,7 +511,7 @@ $reservas=getReservasPendientes($idPrestador);
 
                                     <th scope="col"><?=$lang["valor_total"];?></th>
 
-                                    <th scope="col">Acciones</th>
+                      
 
                                  </tr>
 
