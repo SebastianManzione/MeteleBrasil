d842ddc39ed680a4be7fea6869c2cf5a2619faee
SisteMANZ <admin@sistemanz.com.ar>
2026-01-07 12:48:44 -0300

    fix: centrar modal de login con modal-dialog-centered en mobile

--- includes/navbar.php
+++ includes/navbar.php
@@ -387,7 +387,7 @@ setTimeout(location.reload(), 5000);
 
    <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
   aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
     <div class="modal-content">
       <div class="row">
    <div class="col bg-white ">
