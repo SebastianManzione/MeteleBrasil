80e913315c1fc4bbe665d31ff78af70edd54ded7
SisteMANZ <admin@sistemanz.com.ar>
2026-01-10 00:28:39 -0300

    fix: agregar z-index 9999 para dropdown monedas en Safari mobile

--- includes/navbar.php
+++ includes/navbar.php
@@ -624,7 +624,7 @@ $precioTotalCarrito+=($tarifa[0]["valor"] ?? 0);
     </li>
     <li class="dropdown" style="position: relative;">
       <a class="text-white color-w cursor-size cursor dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="currencyDropdownMobile" data-display="static"><?= $_SESSION["moneda_sel_sym"] ?? 'U$D'; ?></a> 
-     <div class="dropdown-menu menu-civa" aria-labelledby="currencyDropdownMobile" style="position: fixed !important; right: 70px !important; top: 55px !important; left: auto !important; max-width: 250px !important; transform: none !important; margin: 0 !important;">
+     <div class="dropdown-menu menu-civa" aria-labelledby="currencyDropdownMobile" style="position: fixed !important; right: 70px !important; top: 55px !important; left: auto !important; max-width: 250px !important; transform: none !important; margin: 0 !important; z-index: 9999 !important;">
        <?php 
 for ($i=0; $i < count($monedas); $i++) { 
  ?>
