a6ec6a2ac7e29adbb3be30da9243a9786f5ce770
SisteMANZ <admin@sistemanz.com.ar>
2026-01-10 00:20:14 -0300

    fix: forzar alineacion derecha con right 0 y left auto en menu monedas

--- includes/navbar.php
+++ includes/navbar.php
@@ -624,7 +624,7 @@ $precioTotalCarrito+=($tarifa[0]["valor"] ?? 0);
     </li>
     <li class="dropdown" style="position: relative;">
       <a class="text-white color-w cursor-size cursor dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="currencyDropdownMobile"><?= $_SESSION["moneda_sel_sym"] ?? 'U$D'; ?></a> 
-     <div class="dropdown-menu dropdown-menu-right menu-civa" aria-labelledby="currencyDropdownMobile">
+     <div class="dropdown-menu dropdown-menu-right menu-civa" aria-labelledby="currencyDropdownMobile" style="right: 0 !important; left: auto !important;">
        <?php 
 for ($i=0; $i < count($monedas); $i++) { 
  ?>
